94aa256ac3b53ad397dbc2b6febacfc8657e7832
sahilparikh98 <sparikh98@gmail.com>
2016-10-23 00:25:38 -0500

    added custom backend classes, registered Parse instance and notification instance in Application class

---
+++ happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/Post.java
@@ -0,0 +1,49 @@
+package com.jaabs.alanzeng.happsut;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+/**
+ * Created by sparikh98 on 10/22/16.
+ */
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+
+    //private Tags postTags;
+
+    public Post()
+    {
+
+    }
+
+    public void setStartTime(int startTime) {
+        put("startTime", startTime);
+    }
+
+    public void setEndTime(int endTime) {
+        put("endTime", endTime);
+    }
+
+    public void setDescription(String description) {
+        put("description", description);
+    }
+
+    public void setFromOrg(ParseUser fromOrg) {
+        put("fromOrg", fromOrg);
+    }
+
+    public void setLocation(String location) {
+        put("location", location);
+    }
+
+    public void setDate(int date) {
+        put("date", date);
+    }
+
+    public void setName(String name)
+    {
+        put("name", name);
+    }
+}
--- happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/happsUTApplication.java
+++ happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/happsUTApplication.java
@@ -1,11 +1,25 @@
 package com.jaabs.alanzeng.happsut;
 
-//import com.onesignal.OneSignal; implement once running app
+import com.onesignal.OneSignal;
 import android.app.Application;
+import com.parse.Parse;
+
 /**
  * Created by sparikh98 on 10/22/16.
  */
 
+/**
+ * @author sparikh98
+ * a class that creates the main application's internet connected activity
+ */
 public class happsUTApplication extends Application {
+    public void onCreate()
+    {
+        super.onCreate();
+        OneSignal.startInit(this).init();
+        Parse.initialize(new Parse.Configuration.Builder(this).applicationId("happsut1").server("https://happsut-jaabs-server.herokuapp.com/parse").build());
+
+
+    }
 
 }
