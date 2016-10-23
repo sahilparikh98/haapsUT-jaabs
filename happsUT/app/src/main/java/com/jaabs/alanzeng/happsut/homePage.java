20356a65e5ee9369f047fda157069d7f09286fad
Alan Zeng <alanzeng@Alans-MacBook-Pro.local>
2016-10-22 22:09:51 -0500

    TabView

--- happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/homePage.java
+++ happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/homePage.java
@@ -1,13 +1,26 @@
 package com.jaabs.alanzeng.happsut;
 
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class homePage extends AppCompatActivity {
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
     }
+
+
 }
