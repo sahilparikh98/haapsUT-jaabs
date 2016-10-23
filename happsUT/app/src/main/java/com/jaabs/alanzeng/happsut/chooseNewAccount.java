9202d3ac16cd7c80173d57c116d35d7f7e290e76
Bill Yang <longbillyang@yahoo.com>
2016-10-23 02:34:47 -0500

    linked the creating new user account

--- happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/chooseNewAccount.java
+++ happsUT/app/src/main/java/com/jaabs/alanzeng/happsut/chooseNewAccount.java
@@ -1,7 +1,10 @@
 package com.jaabs.alanzeng.happsut;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class chooseNewAccount extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class chooseNewAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_new_account);
+        setUpButtons();
+    }
+
+    private void setUpButtons(){
+        Button newUser = (Button) findViewById(R.id.newUser);
+        newUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(chooseNewAccount.this, createNewAccount.class));
+            }
+        });
     }
 }
